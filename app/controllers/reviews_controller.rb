bef54207754e9c12a27219a8dbd64f680acd7ed3
Makayla <makaylabusher@gmail.com>
2023-01-02 13:58:52 -0600

    add reviews create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,19 @@ class ReviewsController < ApplicationController
   #   render template: "reviews/index"
   # end
 
+  def new 
+    @review = Review.new 
+    render template: "reviews/new"
+  end
+
+  def create
+    @review = Review.new(
+      rating: params[:review][:rating],
+      description: params[:review][:description],
+      user_id: params[:review][:user_id],
+      book_id: params[:review][:book_id]
+    )
+    @review.save
+    redirect_to "/"
+  end
 end
